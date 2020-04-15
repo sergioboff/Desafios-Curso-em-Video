3867f66517044b2d9289bca231ca38f22274162c
Sergio Boff <sergio.boff@gmail.com>
2020-04-15 10:50:04 -0300

    #Numeros Primos

--- Desafio52.py
+++ Desafio52.py
@@ -1,9 +1,15 @@
 #leitura de um inteiro verificação se primo
-
 n = int(input('Digite um valor inteiro: '))
 x = 0
-for i in range(2, n):
-    if n % i ==0:
-        print('Numero não é Primo')
+for i in range(1, n+1):
+    if n % i == 0:
+        print ('\033[34m', end='')
+        x += 1
     else:
-        print('Numero  é primo')
+        print('\033[31m', end='')
+    print ('{}  '.format(i), end='')
+print('\n\033[m')
+if x == 2:
+    print(' O número {} foi divisivel {}x, portanto ele  é Primo'.format(n, x))
+else:
+    print('O número {} foi divisivel {}x, portanto ele não é Primo'.format(n, x))
